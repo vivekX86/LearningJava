68dde9b49ca572a5ae1b1df9807790786cd6b08e
vivekX86 <vivekanandsingh162004@gmail.com>
2025-12-11 17:53:40 +0530

    Folder Structure Change

---
+++ Lab1/rect.java
@@ -0,0 +1,37 @@
+package Lab1;
+import java.util.Scanner;
+
+class Rectangle{
+    float length;
+    float breadth;
+    float area;
+    float parameter;
+
+    void read(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length fo the Rectangle: ");
+        this.length = sc.nextInt();
+        System.out.print("Enter breadth fo the Rectangle: ");
+        this.breadth = sc.nextInt();
+    }
+
+    void calculate(){
+         this.area = this.length * this.breadth;
+         this.parameter = 2*(this.length + this.breadth);
+    }
+
+    void display(){
+        System.out.println("Area of Rectangle is: "+this.area+"\nParameter of Rectangle is: "+this.parameter);
+    }
+}
+
+public class rect {
+    public static void main(String[] args){
+        Rectangle r1 = new Rectangle();
+
+        r1.read();
+        r1.calculate();
+        r1.display();
+
+    }
+}
