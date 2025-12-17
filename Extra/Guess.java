9878a883d48e3e9937d51805ea1327f56d431dd5
vivekX86 <vivekanandsingh162004@gmail.com>
2025-12-17 17:48:54 +0530

    Extra

---
+++ Extra/Guess.java
@@ -0,0 +1,32 @@
+package Extra;
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = (int)(Math.random() * 1000) + 1;
+
+        System.out.println("Guess the number between 1 - 1000 (10 chances)");
+        int chance = 10;
+
+        while (chance > 0) {
+            int guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("It's less");
+            } else if (guess < number) {
+                System.out.println("It's more");
+            } else {
+                System.out.println("FOUND IT! :)");
+                break;
+            }
+            chance--;
+            System.out.println("Chances left: " + chance);
+        }
+        System.out.println("Better luck next time :), the number was "+number);
+
+        sc.close();
+    }
+}
